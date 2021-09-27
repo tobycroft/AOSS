6e0e88e8a50eee8baefaa6e87b6caf16d6ce3626
火线兔 <silverhawk@foxmail.com>
2021-09-27 23:45:08 +0800

    TuuzPC

--- extend/SendFile/SendFile.php
+++ extend/SendFile/SendFile.php
@@ -4,10 +4,10 @@ namespace SendFile;
 
 class SendFile
 {
-    function send($upload_url, $real_path, $file_name)
+    function send($upload_url, $real_path, $mime_type, $file_name)
     {
         $postData = [
-            'file' => new \CURLFile($real_path, '', $file_name)
+            'file' => new \CURLFile($real_path, $mime_type, $file_name)
         ];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $upload_url);
